895de807153effed31ee290b21b0d78c2c3ffad9
alpinstudiosofficial <alpinstudiosofficial@gmail.com>
2026-05-23 21:46:11 +0200

    init: Astro project with Tailwind, TypeScript, React

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+
+export default defineConfig({
+  output: 'static',
+  integrations: [
+    tailwind(),
+    react(),
+  ],
+});
